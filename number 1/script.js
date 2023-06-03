7b25402aecbf09e4ee2d4ac6b74c06a493d5a3da
Safiullova <e.safiullova2000@mail.ru>
2023-06-03 23:50:51 +0400

    done exs 1

---
+++ number 1/script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container'); // переменная для постов
+let postContent = ""; // Переменная для отрисовки карточки (объект поста)
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+
+    .then(json => {
+        for ( let post of json) { // Цикл перебирает данные для каждого поста
+            postContent += `<div class='post'>
+                <h2>${post.title}</h2>
+                <div>${post.body}</div>
+                </div>`;
+        }
+        container.innerHTML = postContent; //Добавляю html-разметку в нужный элемент
+    }
+)
